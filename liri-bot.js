9148725e8e1389b20c3fdfabdd61bdcb85d21de7
erithr <hvillalo@linkedin.biz>
2018-11-07 21:10:00 -0600

    got all functions working exept for the do what it says

--- liri-bot.js
+++ liri-bot.js
@@ -3,112 +3,114 @@ const env = require("dotenv").config();
 const fs = require("fs");
 const keys = require("./keys");
 const Spotify = require('node-spotify-api');
+const moment = require('moment');
 const spotify = new Spotify(keys.spotify);
 const request = require('request');
 const omdb = "f7f10822";
+const bandsintown = keys.bandsintown;
 
 // const bandsintown = require('bandsintown')(APP_ID);
 //get user input
 const input = process.argv[2];
+
 const ask = process.argv[3];
 
 console.log("check below");
 //make a decision based on the command
-switch (input){
-    case "concert-this":
+
+
+    switch (input){
+        case "concert-this":
         concertThis();
         break;
-    case "spotify-this-song":
-    spotifyThisSong();
-    break;
-    case "movie-this":
+        case "spotify-this-song":
+        spotifyThisSong();
+        break;
+        case "movie-this":
         movieThis();
         break;
-    case "do-what-it-says":
-    doWhatItSays();
-    break;
-    default: 
-    console.log("I don't understand, ask Foogle-Bot");
-    break;
+        case "do-what-it-says":
+        doWhatItSays();
+        break;
+        default: 
+        console.log("I don't understand, ask Foogle-Bot");
+        break;
+        
+    }
+    
+function doWhatItSays(){
 
-}
-
-
-
-const doWhatItSays = function(){
-
-    fs.readFile('random.txt', 'utf8', function(err, data){
+    fs.readFile('random.txt', 'utf8', function(err,data){
         if(err) throw err;
         const dataArr = data.split(',');
-
+        console.log(dataArr);
+        
         if (dataArr.length == 2){
-            pick(dataArr[0], dataArr[1]);
-        }else if (dataArr.length == 1){
-            pick(dataArr[0]);
+            input = dataArr[0];
+            ask = dataArr[1];
+            spotifyThisSong(input, ask);
+        }else if(dataArr.length == 1){
+            input= dataArr[0];
+            spotifyThisSong(input);
         }
     });
-}
+} 
+    
 
 function concertThis(){
-//     console.log("Concert This");
-//     bandsintown
-//   .getArtistEventList('Skrillex')
-//   .then(function(events) {
-//     // return array of events
-//   });
-    
+    request('https://rest.bandsintown.com/artists/'+ ask +'/events?app_id="'+ bandsintown +'"', function (error, response, body) {
+        if (!error && response.statusCode ===200){
+
+            let jsonData = JSON.parse(body);
+            for(var i=0; i<jsonData.length; i++){
+            console.log('Name of Venue '+ jsonData[i].venue.name);
+            console.log('Venue Loation '+ jsonData[i].venue.city);
+            console.log('Date of Event '+ moment(jsonData[i].datetime).format('MM/DD/YYYY'));
+            console.log("----------------------------------------------------------")
+            }
+        }
+      });
 }
 
 function movieThis(){
     request('http://www.omdbapi.com/?t='+ ask +'&apikey='+ omdb +'&', function (error, response, body) {
         if (!error && response.statusCode ===200){
-            const jsonData = JSON.parse(body);
-
+            let jsonData = JSON.parse(body);
             console.log('Title ' + jsonData.Title);
             console.log('Year ' + jsonData.Year);
             console.log('IMDB Rating: ' + jsonData.imdbRating);
             console.log('Rotten Tomato Rating: ' + jsonData.Ratings[1].Value);
-            console.log('Country '+ jsonData.Country);
-            console.log('Langauge' + jsonData.Language);
+            console.log('Country of Production '+ jsonData.Country);
+            console.log('Langauge ' + jsonData.Language);
             console.log('Plot ' + jsonData.Plot);
-            console.log('Actors' + jsonData.Actors);
+            console.log('Actors ' + jsonData.Actors);
             
-            // * Title of the movie.
-            // * Year the movie came out.
-            // * IMDB Rating of the movie.
-            // * Rotten Tomatoes Rating of the movie.
-            // * Country where the movie was produced.
-            // * Language of the movie.
-            // * Plot of the movie.
-            // * Actors in the movie.
-
         }
        
       });
 }
 // console.log(JSON.stringify(result, null, 2));
-const getNames = function(artist){
-    return artist.name;
-}
 
 function spotifyThisSong(){
     console.log("Spotify This:"+ process.argv[3]);
     spotify.search({ type: 'track', query: ask }, function(err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
+        
+        const getNames = function(artist){
+            return artist.name;
+        }
+
         const songs = data.tracks.items;
         for(var i=0; i<songs.length; i++){
             console.log(i);
             console.log('artist(s): ' + songs[i].artists.map(getNames));
             console.log('song name: ' + songs[i].name);
             console.log('preview song: ' + songs[i].preview_url);
+            console.log('Album Name: ' +songs[i].album.name);
             console.log('--------------------------------');
         }
       });
 }
-// const artist = Response.tracks.items[0].name;
-
-// console.log(artist);
-
 
